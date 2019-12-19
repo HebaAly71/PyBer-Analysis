80ab14c8d942d7645d98627355284ff0dd6ecb3d
HebaAly71 <47098642+HebaAly71@users.noreply.github.com>
2019-12-18 22:53:22 -0800

    box and whiskers

--- PyBer.py
+++ PyBer.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import statistics 
+import scipy.stats as sts
 #%%
 #Load the city and ride data
 city_data_to_load = "/Users/hebamaly/PyBer-Analysis/Resources/city_data.csv"
@@ -153,4 +154,215 @@ plt.savefig("analysis/Fig1.png")
 # Show the plot
 plt.show()
 
+# %%
+# Get summary statistics
+urban_cities_df.describe()
+# %%
+# Get summary statistics
+suburban_cities_df.describe()
+# %%
+# Get summary statistics
+rural_cities_df.describe()
+# %%
+# Get summary statistics for ride counts.
+urban_ride_count.describe()
+# %%
+# Get summary statistics for ride counts.
+suburban_ride_count.describe()
+# %%
+# Get summary statistics for ride counts.
+rural_ride_count.describe()
+# %%
+# Calculate the mean of the ride count for each city type.
+round(urban_ride_count.mean(),2), round(suburban_ride_count.mean(),2), round(rural_ride_count.mean(),2)
+# %%
+# Calculate the median of the ride count for each city type.
+round(urban_ride_count.median(),2), round(suburban_ride_count.median(),2), round(rural_ride_count.median(),2)
+
+# %%
+#Calculate the mode of the ride count for each city type.
+round(urban_ride_count.mode(),2), round(suburban_ride_count.mode(),2), round(rural_ride_count.mode(),2)
+
+# %%
+# Calculate the measures of central tendency for the ride count for the urban cities.
+mean_urban_ride_count = np.mean(urban_ride_count)
+print(f"The mean for the ride counts for urban trips is {mean_urban_ride_count:.2f}.")
+
+median_urban_ride_count = np.median(urban_ride_count)
+print(f"The median for the ride counts for urban trips is {median_urban_ride_count}.")
+
+mode_urban_ride_count = sts.mode(urban_ride_count)
+print(f"The mode for the ride counts for urban trips is {mode_urban_ride_count}.")
+
+# %%
+# Calculate the measures of central tendency for the ride count for the suburban cities.
+mean_suburban_ride_count = np.mean(suburban_ride_count)
+print(f"The mean for the ride counts for suburban trips is {mean_suburban_ride_count:.2f}.")
+
+median_suburban_ride_count = np.median(suburban_ride_count)
+print(f"The median for the ride counts for suburban trips is {median_suburban_ride_count}.")
+
+mode_suburban_ride_count = sts.mode(suburban_ride_count)
+print(f"The mode for the ride counts for suburban trips is {mode_suburban_ride_count}.")
+
+# %%
+# Calculate the measures of central tendency for the ride count for the rural cities.
+mean_rural_ride_count = np.mean(rural_ride_count)
+print(f"The mean for the ride counts for rural trips is {mean_rural_ride_count:.2f}.")
+
+median_rural_ride_count = np.median(rural_ride_count)
+print(f"The median for the ride counts for rural trips is {median_rural_ride_count}.")
+
+mode_rural_ride_count = sts.mode(rural_ride_count)
+print(f"The mode for the ride counts for rural trips is {mode_rural_ride_count}.")
+
+# %%
+# Get the fares for the urban cities.
+urban_fares = urban_cities_df["fare"]
+urban_fares.head()
+# %%
+# Calculate the measures of central tendency for the average fare for the urban cities.
+mean_urban_fares = np.mean(urban_fares)
+print(f"The mean fare price for urban trips is ${mean_urban_fares:.2f}.")
+
+median_urban_fares = np.median(urban_fares)
+print(f"The median fare price for urban trips is ${median_urban_fares:.2f}.")
+
+mode_urban_fares = sts.mode(urban_fares)
+print(f"The mode fare price for urban trips is {mode_urban_fares}.")
+
+# %%
+# Get the fares for the suburban cities.
+suburban_fares = suburban_cities_df["fare"]
+suburban_fares.head()
+# %%
+# Calculate the measures of central tendency for the average fare for the suburban cities.
+mean_suburban_fares = np.mean(suburban_fares)
+print(f"The mean fare price for suburban trips is ${mean_suburban_fares:.2f}.")
+
+median_suburban_fares = np.median(suburban_fares)
+print(f"The median fare price for suburban trips is ${median_suburban_fares:.2f}.")
+
+mode_suburban_fares = sts.mode(suburban_fares)
+print(f"The mode fare price for suburban trips is {mode_suburban_fares}.")
+# %%
+# Get the fares for the rural cities.
+rural_fares = rural_cities_df["fare"]
+rural_fares.head()
+# %%
+# Calculate the measures of central tendency for the average fare for the rural cities.
+mean_rural_fares = np.mean(rural_fares)
+print(f"The mean fare price for rural trips is ${mean_rural_fares:.2f}.")
+
+median_rural_fares = np.median(rural_fares)
+print(f"The median fare price for rural trips is ${median_rural_fares:.2f}.")
+
+mode_rural_fares = sts.mode(rural_fares)
+print(f"The mode fare price for rural trips is {mode_rural_fares}.")
+
+# %%
+# Get the driver count data from the urban cities.
+urban_drivers = urban_cities_df['driver_count']
+#urban_drivers.head()
+# Get the driver count data from the suburban cities.
+suburban_drivers = suburban_cities_df['driver_count']
+#suburban_drivers.head()
+# Get the driver count data from the rural cities.
+rural_drivers = rural_cities_df['driver_count']
+#rural_drivers.head()
+
+# %%
+# Calculate the measures of central tendency for the average fare for the urban cities.
+mean_urban_drivers = np.mean(urban_drivers)
+print(f"The mean drivers count for urban cities is {mean_urban_drivers:.0f}.")
+
+median_urban_drivers = np.median(urban_drivers)
+print(f"The median drivers count for urban cities is {median_urban_drivers:.0f}.")
+
+mode_urban_drivers = sts.mode(urban_drivers)
+print(f"The mode drivers count for urban cities is {mode_urban_drivers}.")
+
+# %%
+# Calculate the measures of central tendency for the average fare for the suburban cities.
+mean_suburban_drivers = np.mean(suburban_drivers)
+print(f"The mean drivers count for suburban cities is {mean_suburban_drivers:.0f}.")
+
+median_suburban_drivers = np.median(suburban_drivers)
+print(f"The median drivers count for suburban cities is{median_suburban_drivers:.0f}.")
+
+mode_suburban_drivers = sts.mode(suburban_drivers)
+print(f"The mode drivers count for suburban cities is {mode_suburban_drivers}.")
+
+# %%
+# Calculate the measures of central tendency for the average fare for the rural cities.
+mean_rural_drivers = np.mean(rural_drivers)
+print(f"The mean drivers count for rural cities is {mean_rural_drivers:.0f}.")
+
+median_rural_drivers = np.median(rural_drivers)
+print(f"The median drivers count for suburban cities is {median_rural_drivers:.0f}.")
+
+mode_rural_drivers = sts.mode(rural_drivers)
+print(f"The mode drivers count for rural cities is {mode_suburban_drivers}.")
+
+# %%
+# Create a box-and-whisker plot for the urban cities ride count.
+x_labels = ["Urban"]
+fig, ax = plt.subplots()
+ax.boxplot(urban_ride_count, labels=x_labels)
+# Add the title, y-axis label and grid.
+ax.set_title('Ride Count Data (2019)')
+ax.set_ylabel('Number of Rides')
+ax.set_yticks(np.arange(10, 41, step=2.0))
+ax.grid()
+plt.show()
+
+# %%
+# Add all ride count box-and-whisker plots.
+x_labels = ["Urban", "Suburban","Rural"]
+ride_count_data = [urban_ride_count, suburban_ride_count, rural_ride_count]
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.set_title('Ride Count Data (2019)',fontsize=20)
+ax.set_ylabel('Number of Rides',fontsize=14)
+ax.set_xlabel("City Types",fontsize=14)
+ax.boxplot(ride_count_data, labels=x_labels)
+ax.set_yticks(np.arange(0, 45, step=3.0))
+ax.grid()
+# Save the figure.
+plt.savefig("analysis/Fig2.png")
+plt.show()
+
+# %%
+# Get the city that is an outlier for urban ride counts.
+urban_city_outlier = urban_ride_count[urban_ride_count==39].index[0]
+print(f"{urban_city_outlier} has the highest rider count.")
+
+# %%
+# Add all ride fare box-and-whisker plots.
+x_labels = ["Urban", "Suburban","Rural"]
+fares_data = [urban_fares, suburban_fares, rural_fares]
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.set_title('Ride Fare Data (2019)',fontsize=20)
+ax.set_ylabel('Fare($USD)',fontsize=14)
+ax.set_xlabel("City Types",fontsize=14)
+ax.boxplot(fares_data, labels=x_labels)
+ax.set_yticks(np.arange(0, 65, step=3.0))
+ax.grid()
+# Save the figure.
+plt.savefig("analysis/Fig3.png")
+plt.show()
+
+# %%
+# Add all drivers count box-and-whisker plots.
+x_labels = ["Urban", "Suburban","Rural"]
+urban_data = [urban_drivers, suburban_drivers, rural_drivers]
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.set_title('Driver Count Data (2019)',fontsize=20)
+ax.set_ylabel('Number of Drivers)',fontsize=14)
+ax.set_xlabel("City Types",fontsize=14)
+ax.boxplot(urban_data, labels=x_labels)
+ax.set_yticks(np.arange(0, 80, step=3.0))
+ax.grid()
+# Save the figure.
+plt.savefig("analysis/Fig4.png")
+plt.show()
 # %%
