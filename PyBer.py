912fde99959d766163a2527654a6dbbe63cce546
HebaAly71 <47098642+HebaAly71@users.noreply.github.com>
2019-12-18 21:07:48 -0800

    bubble chart

--- PyBer.py
+++ PyBer.py
@@ -53,3 +53,104 @@ pyber_data_df = pd.merge(ride_data_df, city_data_df, how="left", on=["city", "ci
 pyber_data_df.head()
 
 # %%
+#Another way to find data by city type
+pyber_groupbycitytype=pyber_data_df.groupby('type')
+pyber_groupbycitytype_avg=pyber_groupbycitytype['fare','driver_count'].mean()
+#pyber_groupbycitytype
+pyber_groupbycitytype_count=pyber_groupbycitytype['ride_id'].count()
+pyber_bycity_df = pd.merge(pyber_groupbycitytype_avg, pyber_groupbycitytype_count, how="inner", on=["type", "type"])
+pyber_bycity_df.head
+# %%
+# Create the Urban city DataFrame.
+urban_cities_df = pyber_data_df[pyber_data_df["type"] == "Urban"]
+urban_cities_df.head()
+# %%
+# Get the average number of drivers for each urban city.
+urban_driver_count = urban_cities_df.groupby(["city"]).mean()["driver_count"]
+urban_driver_count.head()
+# %%
+# Get the average number of drivers for each city for the suburban and rural cities.
+suburban_driver_count = suburban_cities_df.groupby(["city"]).mean()["driver_count"]
+rural_driver_count = rural_cities_df.groupby(["city"]).mean()["driver_count"]
+# %%
+# Create the Suburban and Rural city DataFrames.
+suburban_cities_df = pyber_data_df[pyber_data_df["type"] == "Suburban"]
+rural_cities_df = pyber_data_df[pyber_data_df["type"] == "Rural"]
+rural_cities_df.head(10)
+# %%
+# Get the number of rides for urban cities.
+urban_ride_count = urban_cities_df.groupby(["city"]).count()["ride_id"]
+urban_ride_count.head()
+
+# %%
+# Create the suburban and rural ride count.
+suburban_ride_count = suburban_cities_df.groupby(["city"]).count()["ride_id"]
+rural_ride_count = rural_cities_df.groupby(["city"]).count()["ride_id"]
+
+# %%
+# Get average fare for each city in the urban cities.
+urban_avg_fare = urban_cities_df.groupby(["city"]).mean()["fare"]
+urban_avg_fare.head()
+
+# %%
+# Get average fare for each city in the suburban and rural cities.
+suburban_avg_fare = suburban_cities_df.groupby(["city"]).mean()["fare"]
+rural_avg_fare = rural_cities_df.groupby(["city"]).mean()["fare"]
+
+# %%
+plt.subplots(figsize=(10, 6))
+plt.scatter(urban_ride_count,
+      urban_avg_fare,
+      s=10*urban_driver_count, c="coral",
+      edgecolor="black", linewidths=1,
+      alpha=0.8, label="Urban")
+plt.title("PyBer Ride-Sharing Data (2019)")
+plt.ylabel("Average Fare ($)")
+plt.xlabel("Total Number of Rides (Per City)")
+plt.grid(True)
+# Add the legend.
+#plt.legend()
+# Build the scatter plots for suburban cities.
+plt.scatter(suburban_ride_count,
+      suburban_avg_fare,
+      s=10*suburban_driver_count, c="skyblue",
+      edgecolor="black", linewidths=1,
+      alpha=0.8, label="Suburban")
+plt.title("PyBer Ride-Sharing Data (2019)")
+plt.ylabel("Average Fare ($)")
+plt.xlabel("Total Number of Rides (Per City)")
+plt.grid(True)
+# Add the legend.
+#plt.legend()
+# Build the scatter plots for rural cities.
+plt.scatter(rural_ride_count,
+      rural_avg_fare,
+      s=10*rural_driver_count, c="gold",
+      edgecolor="black", linewidths=1,
+      alpha=0.8, label="Rural")
+plt.title("PyBer Ride-Sharing Data (2019)")
+plt.ylabel("Average Fare ($)")
+plt.xlabel("Total Number of Rides (Per City)")
+plt.grid(True)
+
+# Incorporate the other graph properties
+plt.title("PyBer Ride-Sharing Data (2019)", fontsize=20)
+plt.ylabel("Average Fare ($)", fontsize=12)
+plt.xlabel("Total Number of Rides (Per City)", fontsize=12)
+plt.grid(True)
+
+# Create a legend
+lgnd = plt.legend(fontsize="12", mode="Expanded",
+         scatterpoints=1, loc="best", title="City Types")
+lgnd.legendHandles[0]._sizes = [75]
+lgnd.legendHandles[1]._sizes = [75]
+lgnd.legendHandles[2]._sizes = [75]
+lgnd.get_title().set_fontsize(12)
+# Incorporate a text label about circle size.
+plt.text(42, 35, "Note:\nCircle size correlates\nwith driver count per city.", fontsize="12")
+# Save the figure.
+plt.savefig("analysis/Fig1.png")
+# Show the plot
+plt.show()
+
+# %%
